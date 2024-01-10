466917930459ee4e7f185de6fe45589fff7e6ba3
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2024-01-10 13:23:59 +0100

    Add project files.

---
+++ PrevodSoustav_IT3A/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PrevodSoustav_IT3A
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void btnPrevod_Click(object sender, RoutedEventArgs e)
+    {
+      string binText = txtVstup.Text;
+      if (JeBinarni(binText))
+      {
+        txtVystup.Text = BinNaDec(binText).ToString();
+      }
+      else
+      {
+        MessageBox.Show("NENI");
+      }
+    }
+
+    private long BinNaDec(string text)
+    {
+      var opacne = text.Reverse().ToArray();
+      long hodnota = 0;
+      for(int i = 0; i < opacne.Length; i++)
+      {
+        hodnota += int.Parse(opacne[i].ToString()) * Convert.ToInt32(Math.Pow(2,i));
+      }
+      return hodnota;
+    }
+
+    private bool JeBinarni(string text)   // 10101010101010
+    {
+      foreach(var znak in text)
+      {
+        if(znak != '1' && znak != '0')
+        {
+          return false;
+        }
+      }
+      return true;
+    }
+  }
+}
